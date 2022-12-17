5efb387c0aa13953bd58797f1238bee8591c74fc
ddshiyu <ddshiyu@163.com>
2022-12-17 23:16:20 +0800

    1

---
+++ src/components/time/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="time m-20">{{t}}</div>
+</template>
+
+<script lang='ts' setup>
+  import { reactive, toRefs, ref, onBeforeMount, onMounted } from 'vue'
+  import dayjs from 'dayjs'
+  let t = ref<string>(dayjs(new Date()).format('hh:mm:ss'))
+  let timer = ref<number>(0)
+  onBeforeMount(() => {
+    console.log('2.组件挂载页面之前执行----onBeforeMount')
+  })
+  onMounted(() => {
+    timer.value = window.setInterval(() => {
+      t.value = dayjs(new Date()).format('hh:mm:ss')
+    }, 1000)
+  })
+</script>
+<style scoped lang='less'>
+.time {
+  font-size: 60px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
+import timecom from '../components/time/Index.vue'
+
 </script>
 
 <template>
-  <main>
+  <main class="d-flex j-center">
     <video autoplay muted loop>
       <source src="@/assets/video.mp4" type="video/mp4">
     </video>
+    <timecom></timecom>
   </main>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 main {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+    position: absolute;
+    z-index: -1;
+  }
 }
-video {
-  width: 100%;
-  height: 100%;
-  object-fit: fill;
-}
-
 </style>
