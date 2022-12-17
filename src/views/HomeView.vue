415254d68713cc3aeb4bff61cd5325f1cc7e8cc4
ddshiyu <ddshiyu@163.com>
2022-12-17 16:27:07 +0800

    添加背景视频

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,22 @@
 
 <template>
   <main>
-    123
+    <video autoplay muted loop>
+      <source src="@/assets/video.mp4" type="video/mp4">
+    </video>
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+}
+
+</style>
